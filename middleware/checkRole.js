c518efe53186a692667e1401cc42cf771a7209c0
Sandeep Tyagi <sandeep@untrain.fit>
2025-05-18 16:55:33 +0530

    new test 55

--- middleware/checkRole.js
+++ middleware/checkRole.js
@@ -1,10 +1,10 @@
 module.exports = (requiredRole) => {
-  console.log("👤 User from token:", req.user);
+  
     return (req, res, next) => {
+      console.log("👤 User from token:", req.user);
       if (!req.user || req.user.role !== requiredRole) {
-        console.log("⛔️ Unauthorized role:", req.user.role);
         return res.status(403).json({ message: 'Access denied: Superadmin only' });
-        
+        console.log("⛔️ Unauthorized role:", req.user.role);
       }
       next();
     };
