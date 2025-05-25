aaef7e0f735cfe02dbef3ccc8f7a7fa785fa8b68
Sandeep Tyagi <sandeep@untrain.fit>
2025-05-25 10:27:00 +0530

    new added range new user 2

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
-const userController = require('../controllers/userController');
+const userController = require('../controllers/UserController');
 const multer = require('multer');
 
 // Image upload setup
