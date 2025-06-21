628071a1fe7027b5a4cbe53ae88e72533e6fed5d
Sandeep Tyagi <sandeep@untrain.fit>
2025-06-21 22:50:49 +0530

    push user forgot password nrew

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { register, login, refreshToken, sendResetLink, resetPassword } = require('../controllers/authController');
+const { register, login, refreshToken, forgotPassword, resetPassword } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/refresh', refreshToken);
-router.post('/forgot-password', sendResetLink);
+router.post('/forgot-password', forgotPassword);
 router.post('/reset-password', resetPassword);
 
 
