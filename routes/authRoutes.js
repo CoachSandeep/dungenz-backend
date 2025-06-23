31f6f079de705a21ac35345452815f5d63adc08d
Sandeep Tyagi <sandeep@untrain.fit>
2025-06-23 14:38:48 +0530

    push user forgot password fdfdfdrr newdfd

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,7 +6,7 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/refresh', refreshToken);
 router.post('/forgot-password', forgotPassword);
-router.post('/reset-password', resetPassword);
+router.post('/reset-password/:token', resetPassword);
 
 
 module.exports = router;
