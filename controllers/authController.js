c8f3ff723c43612772d7cb7a0f74e742041c0ca8
Sandeep Tyagi <sandeep@untrain.fit>
2025-09-24 21:24:00 +0530

    push user active dfd

--- controllers/authController.js
+++ controllers/authController.js
@@ -73,7 +73,7 @@ exports.login = async (req, res) => {
 
     res.json({
       token: accessToken,
-      user: { id: user._id, name: user.name, email: user.email, role: user.role, bio:user.bio, profileImage: user.profileImage, isIndividualProgram:user.isIndividualProgram }
+      user: { id: user._id, name: user.name, email: user.email, role: user.role, bio:user.bio, profileImage: user.profileImage, isIndividualProgram:user.isIndividualProgram, isActive:user.isActive }
     });
   } catch (err) {
     res.status(500).json({ message: err.message });
